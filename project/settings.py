2d10faa7af6c0aabb55632b686806d3256cf6869
unknown <omarwameedh1@gmail.com>
2023-02-23 02:28:28 +0300

    this must me the third commit as schedule

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+
+#apps
+    
+    # 'jobs'
+    # 'blog'
+    # 'contact'
+    # 'home'
+    # 'login'
+
+
+
 ]
 
 MIDDLEWARE = [
